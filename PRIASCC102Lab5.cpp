41c397663bd458c4e19edeb1c0b306084889541a
Cianxzl <ty202500650@wmsu.edu.ph>
2026-02-26 19:23:52 +0800

    Add files via upload

---
+++ PRIASCC102Lab5.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+
+int numStudent, numDays;
+char ans;
+
+do{
+ system("cls");
+
+cout << "====<STUDENT ATTDENDANCE SYSTEM>====";
+
+//INPUT SECTION//
+cout <<"\n\nEnter number of students: ";
+cin >> numStudent;
+
+cout << "Enter number of days: ";
+cin >> numDays;
+
+vector<vector<int>> attendance(numStudent, vector<int>(numDays));
+vector<double> percentage(numStudent);
+vector<int>total(numStudent);
+
+ for(int i = 0; i < numStudent; i++){
+
+  total[i] = 0;
+
+  cout << "\nEnter Student [" << i + 1 << "] attendance:\n";
+
+   for(int j = 0; j < numDays; j++){
+
+     int input;
+
+     while(true){
+     cout << "Day " << j + 1 << ": ";
+     cin >> input;
+
+     if(input== 0 || input == 1){
+     attendance[i][j]= input;
+     total[i] += input;
+     break;
+     }
+
+     else{
+        cout<<"Invalid Input! Enter only 1 or 0.\n";
+     }
+    }
+   }
+
+   percentage[i] = (float(total[i])/ numDays)* 100;
+
+ }
+
+   //TABEL SECTION
+    cout << "\n\n=======================================================================\n";
+    cout << "Student";
+
+    for(int j = 0; j < numDays; j++){
+       cout << "\tDay " << j + 1;
+    }
+
+    cout << "\tTotal";
+    cout << "\tPercentage";
+    cout << "\tRemarks";
+    cout << "\n=======================================================================\n";
+
+    for(int i = 0; i < numStudent; i++){
+     cout << i + 1;
+
+     for(int j = 0; j < numDays; j++){
+      cout << "\t" << attendance[i][j];
+     }
+
+     cout << "\t" << total[i];
+
+     cout << "\t" << percentage[i]<<"%";
+
+     if(percentage[i] < 50){
+
+        cout <<"\t\t DRP";
+     }
+
+     else {
+        cout <<"\t\t OK";
+     }
+
+     cout << endl;
+
+
+    }
+
+    cout<<"\n\nRepeat? (Y/N): ";
+    cin>> ans;
+
+    } while(ans == 'y' || ans == 'Y');
+
+    return 0;
+}
